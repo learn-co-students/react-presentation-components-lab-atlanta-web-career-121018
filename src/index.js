54a19274ee5f87b07d347433e2dc996e96f4201f
github username <github email address>
2019-03-15 01:51:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+let doSomething= (e) => {
+    console.log(e.target)
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={doSomething} />
   </div>,
   document.getElementById('root')
 );
